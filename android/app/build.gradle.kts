106003adbaf869f91299906d96e7e483fbcb0709
Rodrigo Lanes <rodrigolanes@gmail.com>
2025-11-06 12:39:45 -0300

    chore(android): atualiza compileSdk para 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "br.com.rodrigolanes.fizplantao"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
