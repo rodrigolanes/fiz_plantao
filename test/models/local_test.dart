94aba02363368e7194a19f6b2f191e9c5c9ef5b3
Rodrigo Lanes <rodrigolanes@gmail.com>
2025-11-08 13:44:11 -0300

    test: ajustes de formatação nos testes

--- test/models/local_test.dart
+++ test/models/local_test.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:fizplantao/models/local.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Local Model', () {
--- test/models/plantao_test.dart
+++ test/models/plantao_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:fizplantao/models/local.dart';
 import 'package:fizplantao/models/plantao.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Duracao Enum', () {
